db27de94bb4e2974388078dd6fefd3169ea320d2
Aleksandr Semenov <rexsanya1999@gmail.com>
2022-04-24 17:15:04 +0300

    feat: implement dns-stats function

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  let result = {};
+  for (let i = 0; i < domains.length; i++) {
+    let resultKey = '';
+    let domain = [];
+    domain = domains[i].split('.').reverse()
+    for (let j = 0; j < domain.length; j++) {
+      resultKey += `.${domain[j]}`;
+      (result[resultKey] ? result[resultKey]++ : result[resultKey] = 1)
+    }
+  }
+  return result;
 }
 
 module.exports = {
